bf32b3b6251a3db0fa21783738dd88c38d5d413c
DorcasOla <dorcasolatunde89@gmail.com>
2023-10-10 14:07:30 -0700

    0x0D preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of file
+ *
+ * return 0 on success
+*/
+#ifndef MAIN_H
+#define MAIN_H
+int main(void)
+{
+	printf("%s\n",_FILE_);
+	return (0);
+}
+
+#endif
